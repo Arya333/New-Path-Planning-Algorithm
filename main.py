19f7e603a0c77126e8787586b45c50eafbefa85d
Arya Anantula <anantula.arya@gmail.com>
2022-08-05 12:25:45 -0500

    some comments

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import numpy as np
 import collections
 import random
 
-
+# Prints the current state of the grid in the terminal
+# X indicates agent at the coordinate
 def print_grid():
     for row in range(num_rows):
         for col in range(num_cols):
@@ -19,6 +20,8 @@ def print_grid():
                 print("X", end=" ")
         print("")
 
+# Returns true if the coordinate (in parameters) is a coordinate of an agent
+# Used when setting up obstacles randomly
 def in_agents_coords(row, col):
     for agent in agents:
         agent_row = agent.curr_coords[0]
@@ -27,6 +30,8 @@ def in_agents_coords(row, col):
             return True
     return False
 
+# Returns true if the coordinate in parameter already exists in agents_coords_initial array
+# Used when randomly deciding inital positions of agents
 def in_agents_coords_initial(row, col):
     for coord in agents_coords_initial:
         agent_row = coord[0]
@@ -35,6 +40,8 @@ def in_agents_coords_initial(row, col):
             return True
     return False
 
+# Returns true if the coordinate in parameter already exists in list of obstacle coords
+# Used when randomly deciding initial positions of obstacles (and so multiple obstacles don't have same coord)
 def in_obs_coords(row, col):
     for coord in obstacles:
         obs_row = coord[0]
@@ -43,9 +50,14 @@ def in_obs_coords(row, col):
             return True
     return False
 
+# Checks if the provided coordinate is within the bounds of the grid
+# Used during simulation algorithm
 def is_coord_in_bounds(rows, cols, coord):
     return coord[0] >= 0 and coord[0] < rows and coord[1] >= 0 and coord[1] < cols
 
+# Initialize the grid with Coordinate objects for each coord
+# Create the increasing tile number pattern that radiates from the goal
+# Goal has tile number zero. Its surrounding tiles have tile number one and so forth
 def init_grid(rows, cols, goal):
     if (not is_coord_in_bounds(rows, cols, goal)):
         raise Exception("goal coords not in bounds of grid dimensions")
@@ -104,6 +116,7 @@ def init_grid(rows, cols, goal):
 
         dist += 1
 
+# Iterate through the agents list and link the agent to its corresponding Coordinate object
 def init_agents():
     for agent in agents:
         row = agent.curr_coords[0]
@@ -111,6 +124,10 @@ def init_agents():
         grid[row][col].update_agent(agent, False)
         grid[row][col].agent.add_to_path()
 
+# Check if there are agents in the one tile and add these agents to a list
+# Sort this list by an attribute
+# The first agent that is in the list after being sorted is the highest priority agent that will move to the goal first
+# This is the auction algorithm
 def check_ones():
     agents_with_ones = []
     temp = False
@@ -130,6 +147,9 @@ def check_ones():
     # print(agents_with_ones)
     return agents_with_ones[0]
 
+# Checks if an agent is stuck (all surrounding tiles have higher tile numbers)
+# Returns None if no agent is stuck and returns index of stuck agent in agents list if atleast one agent is stuck
+# Used in simulation algorithm to detect failure / stuck agents
 def detect_failure(rows, cols):
     fail = True
     index = 0
@@ -165,7 +185,10 @@ def detect_failure(rows, cols):
         return index
     else:
         return -1
-    
+
+# Checks if an agent at an index is stuck (all surrounding tiles have higher tile numbers)
+# Returns -1 if agent is not stuck and 0 if stuck
+# Used in cause_intermediate function to determine whether or not to begin intermediate target phase
 def detect_failure_agent(rows, cols, index):
     print("det fail agent")
     print(agents)
@@ -186,21 +209,27 @@ def detect_failure_agent(rows, cols, index):
         return -1
     return 0
 
+# Returns true if all agents in agents list are failed agents, if even one is not failed then returns fale
+# Used in simulation algorithm to determine when to move to intermediate targets phase for the stuck agents 
 def cause_intermediate(rows, cols):
     fail = detect_failure_agent(rows, cols, 0)
     temp = 0
-    while (fail != -1 and temp < len(agents)): #problem here
+    while (fail != -1 and temp < len(agents)): 
         fail = detect_failure_agent(rows, cols, temp)
         temp += 1
     return fail != -1
         
+# Main algorithm for moving agents to goal
+# Simulation works until all non-stuck agents reach goal and only stuck agents remain on the grid
+# Then, intermediate target phase begins (in intermediate.py file)
 def simulate(rows, cols, goal, max_num_steps):
     step = 0
     while (step < max_num_steps):
+        # Stop when agents list is empty (meaning all agents reached goal)
         if (not agents):
             break
 
-        # Intermediate goals / delayed stop
+        # Intermediate goal phase
 
         stuck = cause_intermediate(rows, cols)
         if (stuck):
@@ -211,15 +240,16 @@ def simulate(rows, cols, goal, max_num_steps):
             intermediate.create_agent_intermediate_goal_mapping()
             intermediate.simulate()
             break
-
+        
         # Stop at first failure
-
-        # fail = detect_failure(rows, cols)
-        # print("fail = " + str(fail))
-        # if (fail != -1):
-        #     fail_agent_id = agents[fail].get_id()
-        #     plot.failure(fail_agent_id)
-        #     break
+        '''
+        fail = detect_failure(rows, cols)
+        print("fail = " + str(fail))
+        if (fail != -1):
+            fail_agent_id = agents[fail].get_id()
+            plot.failure(fail_agent_id)
+            break
+        '''
 
         step += 1
         agents.sort(key=attrgetter('coord_num'))
@@ -231,6 +261,8 @@ def simulate(rows, cols, goal, max_num_steps):
         num_collisions = len(collisions)
         print(num_collisions)
 
+        # Check if agents are in one tiles for special case
+        # If so, use auction algorithm to determine which agent in one tile moves to goal first
         agent_ones = check_ones()
         if (agent_ones != None):
             curr_coords = agent_ones.get_curr_coords()
@@ -248,6 +280,7 @@ def simulate(rows, cols, goal, max_num_steps):
                 index_remove += 1
             del agents[index_remove]
 
+        # Loop through agents and move them one at a time
         for agent in agents:
             if (agent.get_coord_num() != 1):
                 cur_row = agent.curr_coords[0]
@@ -257,7 +290,8 @@ def simulate(rows, cols, goal, max_num_steps):
                 down = [cur_row + 1, cur_col]
                 left = [cur_row, cur_col - 1]
                 right = [cur_row, cur_col + 1]
-
+                # Move to tile that is available and has lower tile number
+                # If none exist, stay in current tile
                 if (is_coord_in_bounds(rows, cols, up) and grid[up[0]][up[1]].agent == None and grid[up[0]][
                     up[1]].get_num() < agent.get_coord_num()):
                     next_coords[0] = up[0]
@@ -275,6 +309,7 @@ def simulate(rows, cols, goal, max_num_steps):
                     next_coords[0] = right[0]
                     next_coords[1] = right[1]
 
+                # Adding and removing agent pointers from coordinate object if the agent moved positions
                 if (next_coords[0] != cur_row or next_coords[1] != cur_col):
                     new_coord_row = next_coords[0]
                     new_coord_col = next_coords[1]
@@ -286,23 +321,32 @@ def simulate(rows, cols, goal, max_num_steps):
         plot.visualize()
         print("step = " + str(step))
         print(agents)
-        # print_grid()
         print("")
 
-
+'''
+USER INPUT: number of rows for grid
+            number of columns for grid
+            number of agents
+'''
 num_rows = 10
 num_cols = 10
-num_agents = 15
+num_agents = 25
+
+# Velocity for each agent is its id in this case
+# Velocity is the attribute used in auction algorithm to determine which agent moves to the goal
 vels = [i for i in range(1, 1 + num_agents)]
+# USER INPUT: goal coordinate
 goal_coord = list(np.random.randint(1, num_rows, 2))
-#goal_coord = [0, 3]
 grid = [[0 for i in range(num_cols)] for j in range(num_rows)]
 init_grid(num_rows, num_cols, goal_coord)
 
 agent_id_index = 0
+# List of all agents (removed from list when reached goal)
 agents = []
+# List of agents that have reached the goal
 finished_agents = []
 
+# Set up random agent initialization 
 agents_coords_initial = []
 temp = 0
 while temp < num_agents:
@@ -311,19 +355,21 @@ while temp < num_agents:
     if (not (in_agents_coords_initial(rand_row, rand_col) or (rand_row == goal_coord[0] and rand_col == goal_coord[1]))):
         agents_coords_initial.append([rand_row, rand_col])
         temp += 1
-
 for i in range(num_agents):
     agents.append(Agent(agent_id_index, vels[i], agents_coords_initial[i],
                         grid[agents_coords_initial[i][0]][agents_coords_initial[i][1]].get_num(), []))
     agent_id_index += 1
 
+# Sorting agents by the coord_num attribute allows agents closer to the goal to move first
 agents.sort(key=attrgetter('coord_num'))
 init_agents()
 
-num_obstacles = 30
+# USER INPUT: number of obstacles
+num_obstacles = 40
+# Set up obstacles in grid
 obs_num_id = num_rows + num_cols
 temp = 0
-obstacles = []
+obstacles = [] # List of obstacles coordinates
 while temp < num_obstacles:
     rand_row = random.randint(0, num_rows - 1)
     rand_col = random.randint(0, num_cols - 1)
@@ -331,16 +377,18 @@ while temp < num_obstacles:
         grid[rand_row][rand_col] = Coordinate(obs_num_id, [rand_row, rand_col], None, True)
         obstacles.append([rand_row, rand_col])
         temp += 1
-print(obstacles)
 
 print("step = 0")
 print(agents)
-print_grid()
 print("")
 
 plot = Plot(grid, 0)
 plot.visualize()
 start = time.time()
-simulate(num_rows, num_cols, goal_coord, 50)
+# USER INPUT: maximum number of simulation steps
+max_sim_steps = 60
+# SIMULATION 
+simulate(num_rows, num_cols, goal_coord, max_sim_steps)
 print("time taken:", time.time() - start)
+# Create video visualization with ffmpeg by combining images generated by plot object for each time step
 pro = os.system("ffmpeg -r 1 -f image2 -i ./images/step%d.png -s 1000x1000 -y simulation.avi")
