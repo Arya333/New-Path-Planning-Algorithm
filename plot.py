09ed37f0b7cd7abf3c91b0ded56fd7881b72c173
Arya Anantula <anantula.arya@gmail.com>
2022-08-18 03:12:32 -0500

    some changes

--- plot.py
+++ plot.py
@@ -129,11 +129,8 @@ class Plot:
 
     def improved_visualize(self, finished_agents, agents):
         pyplot.figure(figsize=(10, 10))
-        # Set up grid of coordinates with tile numbers on corresponding coordinates
-        # Each tile number will have an associated color (coords with same tile number will be colored the same color for ease of visualization)
         pyplot.imshow(self.grid_nums)
         ax = pyplot.gca()
-        # Set up row and column axis labels
         num_cols = len(self.grid[0])
         x_labels = range(num_cols)
         ax.set_xticklabels(x_labels, fontsize=14)
@@ -145,6 +142,7 @@ class Plot:
         ax.set_xlabel('Column', fontsize=16)
         ax.set_ylabel('Row', fontsize=16)
 
+        # Scale fontsize with size of grid
         agent_fontsize = 11
         grid_size = len(self.grid)
         if (grid_size >= 8):
@@ -154,25 +152,24 @@ class Plot:
         for row in range(len(grid_nums_transposed)):
             for col in range(len(grid_nums_transposed[0])):
                 num = grid_nums_transposed[row][col]
-                # Tile Number
                 pyplot.text(row + .25, col - .25, num,
                             fontweight=850, fontsize=11)
-                # Draw agents as white circles
                 if (self.grid[col][row].agent != None):
                     circle = pyplot.Circle(
                         (row - .05, col + .05), radius=.3, color='white')
+                    # Make agent numbers more visible
                     pyplot.text(row - .3, col + .05, "Agent ",
                                 fontweight=500, fontsize=agent_fontsize)
                     pyplot.text(row - .14, col + .26, self.grid[col][row].agent.get_id(
                     ), fontweight=500, fontsize=agent_fontsize)
                     ax.add_patch(circle)
-                # Draw obstacles as black squares covering the entire coordinate
                 if (self.grid[col][row].obstacle == True):
                     square = pyplot.Rectangle(
                         (row - .51, col - .51), height=1.02, width=1.02, color='black')
                     ax.add_patch(square)
         pyplot.figtext(.025, .05, "step = " + str(self.step), fontsize=15)
 
+        # Create list of agents that reached goal and agents that haven't reached goal
         x_pos = .025
         x_increment = .03
         offset = .185
@@ -188,7 +185,6 @@ class Plot:
         for fagent in finished_agents:
             x_pos += x_increment
             pyplot.figtext(x_pos, .92, fagent.get_id(), fontsize=11)
-        # Save this snapshot of the simulation in the images folder
         pyplot.savefig("images/step" + str(self.step) + ".png")
 
     def improved_failure(self):
@@ -236,11 +232,8 @@ class Plot:
 
     def intermediate_target_visualize(self, finished_agents, agents, intermediate_targets):
         pyplot.figure(figsize=(10, 10))
-        # Set up grid of coordinates with tile numbers on corresponding coordinates
-        # Each tile number will have an associated color (coords with same tile number will be colored the same color for ease of visualization)
         pyplot.imshow(self.grid_nums)
         ax = pyplot.gca()
-        # Set up row and column axis labels
         num_cols = len(self.grid[0])
         x_labels = range(num_cols)
         ax.set_xticklabels(x_labels, fontsize=14)
@@ -261,10 +254,8 @@ class Plot:
         for row in range(len(grid_nums_transposed)):
             for col in range(len(grid_nums_transposed[0])):
                 num = grid_nums_transposed[row][col]
-                # Tile Number
                 pyplot.text(row + .25, col - .25, num,
                             fontweight=850, fontsize=11)
-                # Draw agents as white circles
                 if (self.grid[col][row].agent != None):
                     circle = pyplot.Circle(
                         (row - .05, col + .05), radius=.3, color='white')
@@ -273,16 +264,15 @@ class Plot:
                     pyplot.text(row - .14, col + .26, self.grid[col][row].agent.get_id(
                     ), fontweight=500, fontsize=agent_fontsize)
                     ax.add_patch(circle)
-                # Draw obstacles as black squares covering the entire coordinate
                 if (self.grid[col][row].obstacle == True):
                     square = pyplot.Rectangle(
                         (row - .51, col - .51), height=1.02, width=1.02, color='black')
                     ax.add_patch(square)
+                # Mark the intermediate target tiles
                 if (self.grid[col][row].agent == None and [col, row] in intermediate_targets):
                     pyplot.text(row - .18, col + .15, "IT",
                                 fontweight=400, fontsize=15, color='white')
         pyplot.figtext(.025, .05, "step = " + str(self.step), fontsize=15)
-        pyplot.figtext(.1, .1, "^"+"\n|", fontsize = 23, fontweight = 1000)
         x_pos = .025
         x_increment = .03
         offset = .185
@@ -298,5 +288,4 @@ class Plot:
         for fagent in finished_agents:
             x_pos += x_increment
             pyplot.figtext(x_pos, .92, fagent.get_id(), fontsize=11)
-        # Save this snapshot of the simulation in the images folder
         pyplot.savefig("images/step" + str(self.step) + ".png")
